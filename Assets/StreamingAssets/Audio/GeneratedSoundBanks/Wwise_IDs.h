611e4a42fe7abb9e85b27f369ddc5372699aafbd
Augustin <courcoux.augustin@gmail.com>
2019-05-01 10:57:58 +0200

    Wwise event creation
    no sounds yet

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB = 2959533290U;
+        static const AkUniqueID PLAY_DASH = 2211787386U;
+        static const AkUniqueID PLAY_DEATH = 1172822028U;
+        static const AkUniqueID PLAY_FALLING = 913957623U;
+        static const AkUniqueID PLAY_FTPS = 3079036743U;
+        static const AkUniqueID PLAY_MONSTER1ATTACK = 1185501697U;
+        static const AkUniqueID PLAY_MONSTER1DEATH = 407094821U;
+        static const AkUniqueID PLAY_MONSTER1HIT = 640663108U;
+        static const AkUniqueID PLAY_MONSTER1IDLE = 4031663771U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_PLAYERHIT = 514211712U;
+        static const AkUniqueID PLAYS_SLICING = 92473496U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
